c782c61e8264d571ebbcddcbe4cb242e3c5de4a5
yakSam <sgermein+github@gmail.com>
2019-01-24 10:20:07 +1030

    Create GetYouTubePlayList.js

---
+++ javascripts/GetYouTubePlayList.js
@@ -0,0 +1,13 @@
+debugger;
+$(document).ready(function(){
+var url='https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt-GgMdhwNNqDEn8yKJku0TFOD56-cSac&channelId=UCz22l7kbce-uFJAoaZqxD1A&part=snippet&order=date&maxResults=20';
+var title
+var description
+$.getJSON(url,
+	function(response){
+	title = response.items[0].snippet.title;
+	description = response.items[0].snippet.description;
+	console.log(title)
+	console.log(description)
+});
+});
