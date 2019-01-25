f9165b61db5fe9db5f61c65fe1e6183a6138c2b6
yakSam <sgermein+github@gmail.com>
2019-01-25 16:45:30 +1030

    Update GetYouTubePlayList.js

--- javascripts/GetYouTubePlayList.js
+++ javascripts/GetYouTubePlayList.js
@@ -13,7 +13,7 @@ $.getJSON(url,
 		
 		$.each(response.items, function(key, value){ //first level
 		
-			$.each(value, function(i, j){
+			$.each(value.snippet, function(i, j){
 var title = j.title;
 var description = j.title;
 			});		
