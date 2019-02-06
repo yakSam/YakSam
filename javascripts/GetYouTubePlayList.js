3703794ed52fd65d1cf4970104bf0a6ff386225d
yakSam <sgermein+github@gmail.com>
2019-02-06 16:35:51 +1030

    Update GetYouTubePlayList.js

--- javascripts/GetYouTubePlayList.js
+++ javascripts/GetYouTubePlayList.js
@@ -10,7 +10,8 @@ $.getJSON(url,
 			console.log(value.snippet.title);
 			//var title = jQuery.toArray(value.snippet.title);
 			//$('#show-data p').text(value.snippet.title);
-			$('#show-data p').append(value.snippet.title);	
+			$('#show-data').append('<h3>'value.snippet.title</h3>);	
+			$('#show-data').append('<p>'value.snippet.description</p>);	
 			});
 		});
 	});
