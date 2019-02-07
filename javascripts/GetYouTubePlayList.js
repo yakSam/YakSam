083c840865b0b748a93bb8eb2fa657b8d16225be
Sam Germein <germeisw@unisa.edu.au>
2019-02-07 14:50:14 +1030

    Update GetYouTubePlayList.js

--- javascripts/GetYouTubePlayList.js
+++ javascripts/GetYouTubePlayList.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-var url='https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt-GgMdhwNNqDEn8yKJku0TFOD56-cSac&channelId=UCz22l7kbce-uFJAoaZqxD1A&part=snippet&order=date&maxResults=20';
+var url='https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt-GgMdhwNNqDEn8yKJku0TFOD56-cSac&channelId=UC_EUMYOQE5X76O0WghEMh4Q&part=snippet&order=date&maxResults=20';
 var title
 var description
 $.getJSON(url,
