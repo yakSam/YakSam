a34dcb26c56a5f55befbe50b1688c13a4c152574
yakSam <sgermein+github@gmail.com>
2019-08-01 15:24:25 +0930

    Update GetYouTubePlayList.js

--- javascripts/GetYouTubePlayList.js
+++ javascripts/GetYouTubePlayList.js
@@ -1,25 +1,19 @@
 $(document).ready(function(){
-var form = $('selectChannel');
-
-var url='https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt-GgMdhwNNqDEn8yKJku0TFOD56-cSac&channelId=UC_EUMYOQE5X76O0WghEMh4Q&part=snippet&order=date&maxResults=20';
+var url='https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt-GgMdhwNNqDEn8yKJku0TFOD56-cSac&channelId=UCz22l7kbce-uFJAoaZqxD1A&part=snippet&order=date&maxResults=20';
 var title
 var description
 $.getJSON(url,
 	function(response){
 		
-		$.each(response.items.snippet, function(key, value){ //first level
-		
-			$.each(value, function(i, j){
-var title = j.title;
-var description = j.title;
-			});		
-	title = response.items.snippet.title;
-	description = response.items.snippet.description;
-	console.log(title)
-	console.log(description)
+		$.each(response.items, function(key, value){ //first level
+			
+			console.log(value.snippet.title);
+			//var title = jQuery.toArray(value.snippet.title);
+			//$('#show-data p').text(value.snippet.title);
+			$('#show-data').append('<h3>' + value.snippet.title + '</h3>');	
+			$('#show-data').append('<p>' + value.snippet.description + '</p>');	
+			$('#show-data').append('<iframe type="text/html" src="https://www.youtube.com/embed' + value.id.videoId + '?autoplay=0"></iframe>');
 			});
-
-
 		});
 	});
 
