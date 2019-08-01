4de512135f18fc0af6f9f0fe24cf049d3581b683
yakSam <sgermein+github@gmail.com>
2019-08-01 14:22:34 +0930

    Update GetYouTubePlayList.js

--- javascripts/GetYouTubePlayList.js
+++ javascripts/GetYouTubePlayList.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
+<<<<<<< HEAD
 var form = $('selectChannel');
 
 var url='https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt-GgMdhwNNqDEn8yKJku0TFOD56-cSac&channelId=UC_EUMYOQE5X76O0WghEMh4Q&part=snippet&order=date&maxResults=20';
+=======
+var url='https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt-GgMdhwNNqDEn8yKJku0TFOD56-cSac&channelId=UCz22l7kbce-uFJAoaZqxD1A&part=snippet&order=date&maxResults=20';
+>>>>>>> parent of 083c840... Update GetYouTubePlayList.js
 var title
 var description
 $.getJSON(url,
