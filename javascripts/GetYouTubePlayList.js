dc2fc52267adbd93aa839aa4f906df63962ef70a
yakSam <sgermein+github@gmail.com>
2019-08-06 15:24:20 +0930

    Update GetYouTubePlayList.js

--- javascripts/GetYouTubePlayList.js
+++ javascripts/GetYouTubePlayList.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 var url='https://www.googleapis.com/youtube/v3/search?key=AIzaSyBt-GgMdhwNNqDEn8yKJku0TFOD56-cSac&channelId=UCz22l7kbce-uFJAoaZqxD1A&part=snippet&order=date&maxResults=20';
 var form = $('selectChannel');
-var id = form.options[form.selectedIndex].value;
+var id = $('#channelName').val();
 
 $.getJSON(url,
 	function(response){
